06c6a7779dc5f05123fc8484a29fc29e1597b9b8
ali05-07 <cse24-017@thuto.bac.ac.bw>
2025-10-15 14:54:56 +0200

    Update Transaction class.java

--- Transaction class.java
+++ Transaction class.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.util.Date;
 
 public class Transaction {
@@ -7,9 +9,9 @@ public class Transaction {
     private double amount;
     private double postBalance;
     
-    public Transaction(String type, double amount, double postBalance) {
-        this.transactionId = "TXN" + System.currentTimeMillis();
-        this.date = new Date();
+    public Transaction(String transactionId, Date date, String type, double amount, double postBalance) {
+        this.transactionId = transactionId;
+        this.date = date;
         this.type = type;
         this.amount = amount;
         this.postBalance = postBalance;
@@ -17,8 +19,8 @@ public class Transaction {
     
     @Override
     public String toString() {
-        return String.format("Transaction[%s]: %s - Amount: %.2f, Balance: %.2f", 
-                           transactionId, type, amount, postBalance);
+        return String.format("Transaction[ID: %s, Date: %s, Type: %s, Amount: %.2f, Balance: %.2f]", 
+                           transactionId, date, type, amount, postBalance);
     }
     
     // Getters
@@ -28,3 +30,4 @@ public class Transaction {
     public double getAmount() { return amount; }
     public double getPostBalance() { return postBalance; }
 }
+
