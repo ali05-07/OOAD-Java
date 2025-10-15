497ea5eac985606df45cddb5be83b9568b535de4
ali05-07 <cse24-017@thuto.bac.ac.bw>
2025-10-15 14:51:08 +0200

    Update Customer class.java

--- Customer class.java
+++ Customer class.java
@@ -1,37 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+package model;
 
 public abstract class Customer {
-    protected String customerID;
+    protected String customerId;
     protected String firstName;
     protected String surname;
     protected String address;
     protected String phoneNumber;
-    protected List<Account> accounts;
     
-    public Customer(String customerID, String firstName, String surname, 
-                   String address, String phoneNumber) {
-        this.customerID = customerID;
+    public Customer(String customerId, String firstName, String surname, String address, String phoneNumber) {
+        this.customerId = customerId;
         this.firstName = firstName;
         this.surname = surname;
         this.address = address;
         this.phoneNumber = phoneNumber;
-        this.accounts = new ArrayList<>();
     }
     
-    // Methods from your diagram
-    public List<Account> getAccounts() {
-        return new ArrayList<>(accounts); // Return copy for encapsulation
-    }
-    
-    public void addAccount(Account acc) {
-        accounts.add(acc);
-    }
-    
-    // Getters and setters
-    public String getCustomerID() { return customerID; }
+    // Getters
+    public String getCustomerId() { return customerId; }
     public String getFirstName() { return firstName; }
     public String getSurname() { return surname; }
     public String getAddress() { return address; }
     public String getPhoneNumber() { return phoneNumber; }
+    
+    public abstract String getCustomerType();
 }
+
